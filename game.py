2dff3e4ea78bea1cbb82256a6699fb357b1e957d
MAwais <mawais54013@outlook.com>
2019-11-01 22:50:39 -0700

    play with AI

--- game.py
+++ game.py
@@ -1,19 +1,35 @@
-player1 = input("Player 1, make your move: ")
-player2 = input("Player 2, make your move: ")
+import random 
 
-if player1 == "rock" and player2 == "scissors":
+rand_num = random.randint(0,2)
+if rand_num == 0:
+    computer = "rock"
+elif rand_num == 1:
+    computer = "paper"
+else:
+    computer = "scissors"
+
+player1 = input("Player 1, make your move: ").lower()
+
+if player1 == "rock" and computer == "scissors":
+    print(f"Computer picked {computer}")
     print("player1 wins")
-elif player1 == "rock" and player2 == "paper":
-    print("player2 wins")
-elif player1 == "paper" and player2 == "rock":
+elif player1 == "rock" and computer == "paper":
+    print(f"Computer picked {computer}")
+    print("computer wins")
+elif player1 == "paper" and computer == "rock":
+    print(f"Computer picked {computer}")
     print("Player1 wins")
-elif player1 == "paper" and player2 == "scissors":
-    print("Player2 wins")
-elif player1 == "scissors" and player2 == "rock":
-    print('player2 wins')
-elif player1 == "scissors" and player2 == "paper":
+elif player1 == "paper" and computer == "scissors":
+    print(f"Computer picked {computer}")
+    print("computer wins")
+elif player1 == "scissors" and computer == "rock":
+    print(f"Computer picked {computer}")
+    print('computer wins')
+elif player1 == "scissors" and computer == "paper":
+    print(f"Computer picked {computer}")
     print("Player1 wins")
-elif player1 == player2:
+elif player1 == computer:
+    print(f"Computer picked {computer}")
     print("Tie")
 else:
     print("Something went wrong")
